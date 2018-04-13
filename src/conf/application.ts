8c54e7425d948dadd647fc306883e66406e251cd
Benjamin Daniel <benjamin.daniel@neo9.fr>
2018-04-13 15:22:32 +0200

    docker: Fix docker integration

--- src/conf/application.ts
+++ src/conf/application.ts
@@ -2,7 +2,7 @@ import { Conf } from './index';
 
 const conf: Conf = {
 	http: {
-		port: 6686,
+		port: process.env.PORT || 6686,
 		logLevel: 'dev', // log format for morgan
 		routingController: {
 			classTransformer: false,
